f2418af90aa17b38793b2b44de9031ddb538c5ab
bagdohyeong00 <bagdohyeong00@gmail.com>
2026-07-15 16:31:39 +0900

    add: CORS 및 requirements.txt

--- BE/main.py
+++ BE/main.py
@@ -41,6 +41,8 @@ origins = [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
     "http://localhost:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 TourBase.metadata.create_all(bind=tour_engine)
